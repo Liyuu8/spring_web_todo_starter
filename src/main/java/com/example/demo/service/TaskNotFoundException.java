b0ee03e9fa0962a78f8783cd804c005831581b27
Liyuu <13977245+Liyuu8@users.noreply.github.com>
2020-04-05 20:20:28 +0900

    Implement the dao and service

--- src/main/java/com/example/demo/service/TaskNotFoundException.java
+++ src/main/java/com/example/demo/service/TaskNotFoundException.java
@@ -1,6 +1,6 @@
 package com.example.demo.service;
 
-public class TaskNotFoundException extends RuntimeException{
+public class TaskNotFoundException extends RuntimeException {
 
 	private static final long serialVersionUID = 1L;
 
--- src/main/java/com/example/demo/service/TaskServiceImpl.java
+++ src/main/java/com/example/demo/service/TaskServiceImpl.java
@@ -7,14 +7,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
-import com.example.demo.app.task.TaskForm;
 import com.example.demo.entity.Task;
 import com.example.demo.repository.TaskDao;
 
 @Service
 public class TaskServiceImpl implements TaskService {
 
-		
 	private final TaskDao dao;
 
 	@Autowired
@@ -26,16 +24,16 @@ public class TaskServiceImpl implements TaskService {
 	public List<Task> findAll() {
 		return dao.findAll();
 	}
-	
+
 	@Override
 	public Optional<Task> getTask(int id) {
-		
-		//削除してください
-		Optional<Task> taskOpt = null;
-		return taskOpt;
-		
-		//Optional<Task>一件を取得 idが無ければ例外発生　
 
+		// Optional<Task>一件を取得 idが無ければ例外発生
+		try {
+			return dao.findById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new TaskNotFoundException("指定されたタスクが存在しません");
+		}
 	}
 
 	@Override
@@ -45,17 +43,20 @@ public class TaskServiceImpl implements TaskService {
 
 	@Override
 	public void update(Task task) {
-		
-		//Taskを更新　idが無ければ例外発生
 
+		// Taskを更新 idが無ければ例外発生
+		if (dao.update(task) == 0) {
+			throw new TaskNotFoundException("更新するタスクが存在しません");
+		}
 	}
 
 	@Override
 	public void deleteById(int id) {
-		
-		//Taskを更新 idがなければ例外発生
 
+		// Taskを更新 idがなければ例外発生
+		if (dao.deleteById(id) == 0) {
+			throw new TaskNotFoundException("削除するタスクが存在しません");
+		}
 	}
-
 
 }
